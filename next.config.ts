6b40e72afa1f6324ecfd4bcd87ea733e3f6d26e5
Vinay Sharma <vinayjnvk2002@gmail.com>
2025-05-31 16:31:57 +0000

    I ran the command to build the project,k i left with and unsuccessfull build, here is the build log, i want you check and fir the errors
    ./node_modules/@opentelemetry/otlp-grpc-exporter-base/build/src/util.js:23:1
    Module not found: Can't resolve 'fs'
      21 | const path = require("path");
      22 | const url_1 = require("url");
    > 23 | const fs = require("fs");
         | ^
      24 | const otlp_exporter_base_1 = require("@opentelemetry/otlp-exporter-base");
      25 | const grpc_exporter_transport_1 = require("./grpc-exporter-transport");
      26 | exports.DEFAULT_COLLECTOR_URL = 'http://localhost:4317';
    
    https://nextjs.org/docs/messages/module-not-found
    
    Import trace for requested module:
    ./node_modules/@opentelemetry/otlp-grpc-exporter-base/build/src/index.js
    ./node_modules/@opentelemetry/exporter-trace-otlp-grpc/build/src/OTLPTraceExporter.js
    ./node_modules/@opentelemetry/exporter-trace-otlp-grpc/build/src/index.js
    ./node_modules/@opentelemetry/sdk-node/build/src/TracerProviderWithEnvExporter.js
    ./node_modules/@opentelemetry/sdk-node/build/src/sdk.js
    ./node_modules/@opentelemetry/sdk-node/build/src/index.js
    ./src/instrumentation.ts
    
    ./node_modules/hexer/hex_spy.js:2:1
    Module not found: Can't resolve 'stream'
      1 | var util = require('util');
    > 2 | var Transform = require('stream').Transform;
        | ^
      3 | var ChunkedHexTransform = require('./chunked_hex_transform');
      4 |
      5 | module.exports = HexSpy;
    
    https://nextjs.org/docs/messages/module-not-found
    
    Import trace for requested module:
    ./node_modules/hexer/index.js
    ./node_modules/bufrw/interface.js
    ./node_modules/bufrw/index.js
    ./node_modules/thriftrw/index.js
    ./node_modules/jaeger-client/dist/src/thrift.js
    ./node_modules/@opentelemetry/exporter-jaeger/build/src/types.js
    ./node_modules/@opentelemetry/exporter-jaeger/build/src/jaeger.js
    ./node_modules/@opentelemetry/exporter-jaeger/build/src/index.js
    ./src/instrumentation.ts
    
    > Build failed because of webpack errors

--- next.config.ts
+++ next.config.ts
@@ -21,7 +21,13 @@ const nextConfig: NextConfig = {
   },
   // Corrected: serverExternalPackages should be a top-level property.
   // This informs Next.js how to handle these packages in server environments.
-  serverExternalPackages: ['@opentelemetry/exporter-jaeger', '@grpc/grpc-js'],
+  serverExternalPackages: [
+    '@opentelemetry/exporter-jaeger',
+    '@grpc/grpc-js',
+    '@opentelemetry/sdk-node', // Added: Core SDK for Node.js, can pull in various exporters
+    '@opentelemetry/otlp-grpc-exporter-base', // Added: Source of 'fs' module not found
+    'jaeger-client', // Added: Dependency of exporter-jaeger, source of 'stream' module not found via 'hexer'
+  ],
 };
 
 export default nextConfig;
